08a983dd48e5ff24bcebe27a574a8c33c9cf0851
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-05-11 17:24:13 -0400

    added hooks

---
+++ frontend/components/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+
+import useLocalStorage from "./useLocalStorage";
+
+
+
+const useDarkMode= (initialValue) =>{
+    const [darkMode,setDarkMode] = useLocalStorage("darkmode" ,initialValue);
+    return [darkMode,setDarkMode];
+};
+
+export default useDarkMode;
